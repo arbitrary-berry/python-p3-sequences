acbe497bc996da8edc91658f3bfca73c0b41cc35
Alyssa <alyssa.brooksong@gmail.com>
2023-07-25 16:07:10 -0700

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,12 @@
 
 def print_fibonacci(length):
     pass
+    fibonacci = []
+    if length > 0:
+        fibonacci.append(0)
+        if length > 1:
+            fibonacci.append(1)
+            if length > 2:
+                for i in range(2, length):
+                    fibonacci.append(fibonacci[i - 1] + fibonacci[i - 2])
+    print(fibonacci)
